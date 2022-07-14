17e8ff232778ecdf74b821df97ecb6722ddfb5d2
unknown <a_bcom@hotmail.com>
2022-07-14 23:48:00 +0300

    text changed

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Reactnwith Ali Binmahfodh
+          Learn Reactn with Ali Binmahfodh
         </a>
       </header>
     </div>
